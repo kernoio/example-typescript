83a3cfa699eab6d9addf91ce305d605144ca2092
saguilar <sergi.ruiz@kernio.io>
2025-10-28 18:33:28 +0100

    fix things

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,13 +3,11 @@ import tagsController from './tag/tag.controller';
 import articlesController from './article/article.controller';
 import authController from './auth/auth.controller';
 import profileController from './profile/profile.controller';
-import demoController from './demo/demo.controller';
 
 const api = Router()
   .use(tagsController)
   .use(articlesController)
   .use(profileController)
   .use(authController)
-  .use(demoController);
 
 export default Router().use('/api', api);
