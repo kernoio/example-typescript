763ac0c9a9107e8da7a8d30f4a9a121b3f368eb8
Jordan Bergin <jordan.bergin@kerno.io>
2026-09-25 15:03:12 +0100

    Add GET /api/stats/tags

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,11 +3,13 @@ import tagsController from './tag/tag.controller';
 import articlesController from './article/article.controller';
 import authController from './auth/auth.controller';
 import profileController from './profile/profile.controller';
+import statsController from './stats/stats.controller';
 
 const api = Router()
   .use(tagsController)
   .use(articlesController)
   .use(profileController)
   .use(authController)
+  .use(statsController)
 
 export default Router().use('/api', api);
